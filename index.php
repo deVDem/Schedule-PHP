bbd70b9b16a772160d3bdfef788e82bf3ab4bb34
Roman Medvedev <39949986+devdem@users.noreply.github.com>
2020-11-30 18:15:38 +0300

    fixes

--- index.php
+++ index.php
@@ -9,11 +9,11 @@ $action = getRes('action');
 $response = array(); // answer from server
 $credinals = json_decode(file_get_contents("D:\Sites\credinals.json"), true);
 $connect = mysqli_connect(
-    $credinals['mySQL']['host'],
-    $credinals['mySQL']['account']['user'],
-    $credinals['mySQL']['account']['password'],
-    $credinals['mySQL']['account']['database'],
-    $credinals['mySQL']['port']);
+    $credinals["mySQL"]["host"],
+    $credinals["mySQL"]["account"][0]["user"],
+    $credinals["mySQL"]["account"][0]["password"],
+    $credinals["mySQL"]["account"][0]["database"],
+    $credinals["mySQL"]["port"]);
 if (!$connect) {
     goError("Error on database server", 0x01);
 } else {
